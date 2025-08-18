cfdd2b2c0847a2d7bced60f9b86256ea7e1681c3
Joan Sardans Pla <joansardanspla@protonmail.com>
2025-08-18 17:23:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+ const btnTop = document.getElementById('btn-top');
 
+  // Mostrar o amagar el botó quan fas scroll
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      btnTop.classList.add('show');
+      btnTop.style.display = 'block';
+    } else {
+      btnTop.classList.remove('show');
+      btnTop.style.display = 'none';
+    }
+  });
+
+  // Quan cliquis el botó, scroll suau cap a dalt de tot
+  btnTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
